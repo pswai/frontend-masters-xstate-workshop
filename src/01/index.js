79732e54b19b210ecd6f5bc5d75a1b063a5c5446
PSWai <parksoon_my@hotmail.com>
2021-09-23 13:23:57 +0800

    [Ex01] Simple fsm with switch

--- src/01/index.js
+++ src/01/index.js
@@ -1,13 +1,25 @@
-const elBox = document.querySelector('#box');
+const elBox = document.querySelector("#box");
 
 // Pure function that returns the next state,
 // given the current state and sent event
 function transition(state, event) {
-  switch (
-    state
+  switch (state) {
     // Add your state/event transitions here
     // to determine and return the next state
-  ) {
+    case "active":
+      switch (event) {
+        case "TOGGLE":
+          return "inactive";
+      }
+      break;
+
+    case "inactive":
+    default:
+      switch (event) {
+        case "TOGGLE":
+          return "active";
+      }
+      break;
   }
 }
 
@@ -16,10 +28,12 @@ let currentState = undefined;
 
 function send(event) {
   // Determine the next value of `currentState`
+  currentState = transition(currentState, event);
 
   elBox.dataset.state = currentState;
 }
 
-elBox.addEventListener('click', () => {
+elBox.addEventListener("click", () => {
   // send a click event
+  send("TOGGLE");
 });
