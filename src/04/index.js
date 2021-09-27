16d0fec1dbff4d911d2e9eed00cbdf2844adde03
PSWai <parksoon_my@hotmail.com>
2021-09-27 15:05:42 +0800

    [Ex04] Extract action into named function

--- src/04/index.js
+++ src/04/index.js
@@ -2,34 +2,38 @@ import { createMachine, interpret } from "xstate";
 
 const elBox = document.querySelector("#box");
 
-const setPoint = (context, event) => {
-  // Set the data-point attribute of `elBox`
-  // ...
-  elBox.dataset.point = `${event.clientX}, ${event.clientY}`;
-};
-
-const machine = createMachine({
-  initial: "idle",
-  states: {
-    idle: {
-      on: {
-        mousedown: {
-          // Add your action here
-          // ...
-          target: "dragging",
-          actions: setPoint,
+const machine = createMachine(
+  {
+    initial: "idle",
+    states: {
+      idle: {
+        on: {
+          mousedown: {
+            // Add your action here
+            // ...
+            target: "dragging",
+            actions: "setPoint",
+          },
         },
       },
-    },
-    dragging: {
-      on: {
-        mouseup: {
-          target: "idle",
+      dragging: {
+        on: {
+          mouseup: {
+            target: "idle",
+          },
         },
       },
     },
   },
-});
+  {
+    actions: {
+      setPoint: (context, event) => {
+        // Set the data-point attribute of `elBox`
+        elBox.dataset.point = `${event.clientX}, ${event.clientY}`;
+      },
+    },
+  }
+);
 
 const service = interpret(machine);
 
