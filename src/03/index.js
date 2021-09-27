e7dc2300b6990ace604137c254ef62aab3a5e5b8
PSWai <parksoon_my@hotmail.com>
2021-09-27 14:08:44 +0800

    [Ex03] Simple machine to handle mouse events

--- src/03/index.js
+++ src/03/index.js
@@ -1,28 +1,45 @@
-import { createMachine, interpret } from 'xstate';
+import { createMachine, interpret } from "xstate";
 
-const elBox = document.querySelector('#box');
+const elBox = document.querySelector("#box");
 
 const machine = createMachine({
-  // Create your state machine here
-  // ...
+  initial: "inactive",
+  states: {
+    inactive: {
+      on: {
+        mousedown: "active",
+      },
+    },
+    active: {
+      on: {
+        mouseup: "inactive",
+      },
+    },
+  },
 });
 
 // Create a service using interpret(...)
-const service = undefined;
+const service = interpret(machine);
 
 // Listen to state transitions and set
 // `elBox.dataset.state` to the state value as before.
 // ...
+service.onTransition((state) => {
+  elBox.dataset.state = state.value;
+});
 
 // Start the service.
 // ...
+service.start();
 
-elBox.addEventListener('mousedown', (event) => {
+elBox.addEventListener("mousedown", (event) => {
   // Send a mousedown event
   // ...
+  service.send(event);
 });
 
-elBox.addEventListener('mouseup', (event) => {
+elBox.addEventListener("mouseup", (event) => {
   // Send a mouseup event
   // ...
+  service.send(event);
 });
