f5ee8bf493f12240127a21db30f1ebdfd10eb77b
PSWai <parksoon_my@hotmail.com>
2021-09-27 14:00:58 +0800

    [Ex02] Refactor machine from Ex01 to use XState

--- src/02/index.js
+++ src/02/index.js
@@ -1,20 +1,34 @@
-import { createMachine } from 'xstate';
+import { createMachine } from "xstate";
 
-const elBox = document.querySelector('#box');
+const elBox = document.querySelector("#box");
 
 const machine = createMachine({
-  // Add your object machine definition here
+  initial: "inactive",
+  states: {
+    inactive: {
+      on: {
+        TOGGLE: "active",
+      },
+    },
+    active: {
+      on: {
+        TOGGLE: "inactive",
+      },
+    },
+  },
 });
 
 // Change this to the initial state
-let currentState = undefined;
+let currentState = machine.initialState;
 
 function send(event) {
   // Determine and update the `currentState`
+  currentState = machine.transition(currentState, event);
 
   elBox.dataset.state = currentState.value;
 }
 
-elBox.addEventListener('click', () => {
+elBox.addEventListener("click", () => {
   // Send a click event
+  send("TOGGLE");
 });
