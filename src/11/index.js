8939737b032373e754346f3352202ec808267a3d
pswai <parksoon_my@hotmail.com>
2021-10-01 07:26:21 +0800

    [Ex11] Parallel states

--- src/11/index.js
+++ src/11/index.js
@@ -1,16 +1,16 @@
-import { createMachine, interpret } from 'xstate';
+import { createMachine, interpret } from "xstate";
 
-const elApp = document.querySelector('#app');
-const elOffButton = document.querySelector('#offButton');
-const elOnButton = document.querySelector('#onButton');
-const elModeButton = document.querySelector('#modeButton');
+const elApp = document.querySelector("#app");
+const elOffButton = document.querySelector("#offButton");
+const elOnButton = document.querySelector("#onButton");
+const elModeButton = document.querySelector("#modeButton");
 
 const displayMachine = createMachine({
-  initial: 'hidden',
+  initial: "hidden",
   states: {
     hidden: {
       on: {
-        TURN_ON: 'visible',
+        TURN_ON: "visible",
       },
     },
     visible: {
@@ -19,24 +19,60 @@ const displayMachine = createMachine({
       // - brightness (bright or dim)
       // See the README for how the child states of each of those
       // parallel states should transition between each other.
+      type: "parallel",
+      states: {
+        mode: {
+          initial: "light",
+          states: {
+            light: {
+              on: {
+                SWITCH: "dark",
+              },
+            },
+            dark: {
+              on: {
+                SWITCH: "light",
+              },
+            },
+          },
+        },
+        brightness: {
+          initial: "bright",
+          states: {
+            bright: {
+              after: {
+                5000: "dim",
+              },
+            },
+            dim: {
+              on: {
+                SWITCH: "bright",
+              },
+            },
+          },
+        },
+      },
+      on: {
+        TURN_OFF: "hidden",
+      },
     },
   },
 });
 
 const displayService = interpret(displayMachine)
   .onTransition((state) => {
-    elApp.dataset.state = state.toStrings().join(' ');
+    elApp.dataset.state = state.toStrings().join(" ");
   })
   .start();
 
-elOnButton.addEventListener('click', () => {
-  displayService.send('TURN_ON');
+elOnButton.addEventListener("click", () => {
+  displayService.send("TURN_ON");
 });
 
-elOffButton.addEventListener('click', () => {
-  displayService.send('TURN_OFF');
+elOffButton.addEventListener("click", () => {
+  displayService.send("TURN_OFF");
 });
 
-elModeButton.addEventListener('click', () => {
-  displayService.send('SWITCH');
+elModeButton.addEventListener("click", () => {
+  displayService.send("SWITCH");
 });
